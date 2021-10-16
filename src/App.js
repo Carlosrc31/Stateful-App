df5aaa318694fd983f5fb3fc0c31b97153117a06
Carlosrc31 <charlie31rc@gmail.com>
2021-10-16 00:33:24 -0500

    Cambios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+  const [name, setName] = useState(()=>{
+    const value = localStorage.getItem("name");
+    const InitValue = JSON.parse(value);
+    return InitValue || "";
+  });
+  
+  const handleInput = (event) => {
+    console.log(event.target.value);
+    setName(event.target.value);
+  }
+
+  useEffect( () => {
+    localStorage.setItem("name",  JSON.stringify(name));
+  }, [name]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+        <form className="form">
+          <label className="form_label">
+            Ingresa tu nombre: 
+          </label>
+          <input
+          type="text"
+          value={name}
+          placeholder="Ingresa tu nombre"
+          onChange={handleInput}
+          className="form_input"
+          >
+          </input>
+          <button type="submit" className="form_button">
+            Recarga
+          </button>
+          <p className="form_p">
+            "Hello, {name}!!!, you're testing a new feature." 
+          </p>
+        </form>
+    </React.Fragment>
   );
 }
 
